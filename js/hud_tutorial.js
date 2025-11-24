6d9c2efe8e89a2a7517d1848c884365648a90873
Florencia <florenciabelenfigueroa05@gmail.com>
2025-11-23 22:03:14 -0300

    Agregado de fondo negro a Tutorial juego

--- js/hud_tutorial.js
+++ js/hud_tutorial.js
@@ -2,24 +2,38 @@
 
 const tutorialHudContainer = new PIXI.Container();
 
-// capa y posicion ----------------------------------------------
+// Capa y posicion --------------------------------------------------
+
 world.sortableChildren = true; 
 tutorialHudContainer.zIndex = 1000;
 
 tutorialHudContainer.x = WORLD_WIDTH / 2; 
 tutorialHudContainer.y = WORLD_HEIGHT / 2;
+
 world.addChild(tutorialHudContainer);
 
 
-// Carga de imagen -----------------------------------------------
+// Capa fondo negro -------------------------------------------------
+const fondoOscuro = new PIXI.Graphics();
+
+fondoOscuro.beginFill(0x000000, 0.5); 
+fondoOscuro.drawRect(-WORLD_WIDTH / 2, -WORLD_HEIGHT / 2, WORLD_WIDTH, WORLD_HEIGHT);
+fondoOscuro.endFill();
+fondoOscuro.eventMode = 'static'; 
+
+tutorialHudContainer.addChild(fondoOscuro);
+
+
+// Carga de imagen --------------------------------------------------
 const tutorial = PIXI.Sprite.from("images/tutorial.png");
 tutorial.width = 1920;
 tutorial.height = 1080;
 tutorial.anchor.set(0.5);
+
 tutorialHudContainer.addChild(tutorial);
 
 
-// Boton y propiedades -------------------------------------------
+// Boton y propiedades ----------------------------------------------
 const botonCerrarContainer = new PIXI.Container();
 
 const botonFondo = new PIXI.Graphics();
@@ -37,13 +51,13 @@ const textoJugar = new PIXI.Text('Jugar', estiloTexto);
 textoJugar.anchor.set(0.5); 
 botonCerrarContainer.addChild(textoJugar);
 
-botonCerrarContainer.y = 380;
 
+// Boton y eventos --------------------------------------------------
+botonCerrarContainer.y = 380; // Ajusta esto si quieres subirlo o bajarlo
 botonCerrarContainer.eventMode = 'static'; 
 botonCerrarContainer.cursor = 'pointer'; 
 
 botonCerrarContainer.on('pointerover', () => {
-    // Teñimos el fondo de ROJO
     botonFondo.tint = 0x8C1B24; 
 });
 
@@ -51,10 +65,9 @@ botonCerrarContainer.on('pointerout', () => {
     botonFondo.tint = 0xFFFFFF; 
 });
 
-
-// Funcion boton agregado del mismo ------------------------------
 botonCerrarContainer.on('pointerdown', () => {
     world.removeChild(tutorialHudContainer);
+    
     window.juegoIniciado = true; 
     console.log("Tutorial cerrado. ¡Tiempo corriendo!");
 });
